ec0d4b445ffd88872d7100df173493bcc12ca915
Pramod Mukane <108333259+Pramod5858@users.noreply.github.com>
2023-08-08 18:08:25 +0530

    Login & Signup

---
+++ src/Component/Login.js
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const Login = () => {
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+
+  const HandleSubmit = async ({ email, password }) => {
+
+    if (!email && !password) {
+      return console.log("it should not be empty")
+    }
+    const url = "http://localhost:5500/login";
+    const result = await fetch(url, {
+      method: "post",
+      body: JSON.stringify({ email, password }),
+      headers: { "content-Type": "application/json" }
+    })
+    let respond = await result.json();
+    console.log(respond);
+    if (respond.authtoken) {
+      console.log(email);
+      console.log(password);
+      alert("You have successfully logged in");
+      window.location.href = "/";
+    } else {
+
+      alert("Not able to logged in, email or password is not correct");
+      window.location.href = "/login";
+    }
+    return (
+      <div>
+        <div>hi {email}</div>
+        <div>hello {password}</div>
+      </div>
+    )
+
+  }
+
+  return (
+    <div className="alert alert-primary" role="alert">
+      <div className='row'>
+        <div className='col'>
+          <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img src="./images/image3.jpg" className="h-100 d-inline-block w-100" alt="Not found image1" />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/image4.jpg" className="h-100 d-inline-block w-100" alt="Not found image2" />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/image5.jpg" className="h-100 d-inline-block w-100" alt="Not found image3" />
+              </div>
+              <div className="carousel-item">
+                <img src="./images/image6.jpg" className="h-100 d-inline-block w-100" alt="Not found image3" />
+              </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
+        <div className='col'>
+
+          <Form>
+            <Form.Group className="mb-3" >
+
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword2">
+              <Form.Label >Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+
+            <Button className="btn btn-success" variant="primary" type="button" disabled={email === "" || password === ""} onClick={() => HandleSubmit({ email, password })}>
+              Submit
+            </Button>
+
+            <button type="button" className="btn btn-link" data-bs-toggle="modal" data-bs-target="#exampleModal1">Forgotten password?</button>
+
+            <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" >Create New Account</button>
+          </Form>
+
+        </div>
+
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Sign Up</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <HandleModal />
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <p><a href="/signup" className="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Create Free Bussiness Account</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="btn modal fade" id="exampleModal1" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Find Your Account</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <Forgottenpassword />
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                {/* <button type="button" className="btn btn-primary" onClick={()=>HandleForgottenpassword()}>Save changes</button> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
+
+function HandleModal() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const HandleSignupRegisteration = async ({ name, email, password }) => {
+    console.log("You cllick:- " + name, email, password);
+
+    const url = "http://localhost:5500/signup";
+    const result = await fetch(url, {
+      method: "post",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    let respond = await result.json()
+    if (!respond) {
+      alert("Something missing or try again")
+      window.location.href = "/login";
+    } else {
+      alert("Successfully created new account and know you can login")
+      window.location.href = "/login";
+    }
+  }
+
+  return (
+    <div className="alert alert-primary" role="alert">
+      <Form>
+
+        <h1>Register</h1>
+        <Form.Group className="mb-3" >
+
+          <Form.Label>Name1</Form.Label>
+          <Form.Control type="text" placeholder="Enter email" value={name} onChange={(e) => setName(e.target.value)} />   {/* //Here value ={name}, so name is state over here */}
+
+
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword1">
+
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </Form.Group>
+
+        <Button className="btn btn-success" type="button" disabled={name ==="" || email==="" || password==="" } onClick={() => { HandleSignupRegisteration({ name, email, password }) }} >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+function Forgottenpassword() {
+  const [email, setEmail] = useState("")
+
+  const HandleForgottenpassword = ({ email }) => {
+    console.log("YouClicked:- " + email);
+  }
+  return (
+    <div>
+      <Form>
+
+        <h1>Register</h1>
+        <Form.Group className="mb-3" >
+
+          <Form.Label>Please enter your email address to search for your account.</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        </Form.Group>
+
+        <Button className="btn btn-success" type="button" onClick={() => { HandleForgottenpassword({ email }) }} >
+          Submit
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+//import { useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
@@ -7,65 +7,78 @@ const SignUp = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
 
-  useEffect(() => {
-    const auth = localStorage.getItem('user');
-    if (auth) {
-      navigate("/login")
-    }
+  // useEffect(() => {
+  //   const auth = localStorage.getItem('user');
+  //   if (auth) {
+  //     navigate("/login")
+  //   }
 
-  },[])
+  // },[])
 
 
-//   const collectData = async () => {
-//     console.log(name, email, password);
-//     let result = await fetch('https://addmngbackend.onrender.com/registers', {
-//       method: 'post',
-//       body: JSON.stringify({ name, email, password }),
-//       headers: {
-//         'content-Type': 'application/json'
-//       },
-//     });
-//     result = await result.json()
-//     console.log(result);
-//     if (result.auth) {
-//       localStorage.setItem("user", JSON.stringify(result.result));
-//       localStorage.setItem("token", JSON.stringify(result.auth));
+  //   const collectData = async () => {
+  //     console.log(name, email, password);
+  //     let result = await fetch('https://addmngbackend.onrender.com/registers', {
+  //       method: 'post',
+  //       body: JSON.stringify({ name, email, password }),
+  //       headers: {
+  //         'content-Type': 'application/json'
+  //       },
+  //     });
+  //     result = await result.json()
+  //     console.log(result);
+  //     if (result.auth) {
+  //       localStorage.setItem("user", JSON.stringify(result.result));
+  //       localStorage.setItem("token", JSON.stringify(result.auth));
 
-//       navigate("/login");
-//     }
-//     else {
-//       alert("Please enter the  here auth is missing details")
-//     }
+  //       navigate("/login");
+  //     }
+  //     else {
+  //       alert("Please enter the  here auth is missing details")
+  //     }
 
-//   }
+  //   }
+
+
+  const HandleSubmit = ({name,email,password}) => {
+    console.log("You click HandleSubmit:- " + name,email,password);
+  }
+
   return (
     <div className="alert alert-primary" role="alert">
-      <Form>
+      <div className="row">
+        <div className="col">
+          <Form>
 
-        <h1>Register</h1>
-        <Form.Group className="mb-3" >
+            <h1>Admin Register</h1>
+            <Form.Group className="mb-3" >
 
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter email" value={name} onChange={(e) => setName(e.target.value)} />   {/* //Here value ={name}, so name is state over here */}
-          
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter email" value={name} onChange={(e) => setName(e.target.value)} />   {/* //Here value ={name}, so name is state over here */}
 
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-        </Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-        <Form.Group className="mb-3" controlId="formBasicPassword1">
+            </Form.Group>
 
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword1">
 
-        <Button className="btn btn-success" type="button" >
-          Submit
-        </Button>
-      </Form>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
+
+            <Button className="btn btn-success" type="button" disabled={name === "" || email === "" || password === ""} onClick={() => { HandleSubmit({name,email,password}) }}>
+              Submit
+            </Button>
+          </Form>
+        </div>
+        <div className="col">
+          <img src="./images/GST7.jpg" className="d-block w-100" alt="Not found image4" />
+        </div>
+      </div>
     </div>
   );
 }
