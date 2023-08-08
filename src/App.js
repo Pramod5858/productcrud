4f5fc176128a2dfe935896f098cd54becd385217
Pramod Mukane <108333259+Pramod5858@users.noreply.github.com>
2023-08-08 19:05:09 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import Home from './Component/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './Component/Navbar';
 import Add from './Component/Add';
-import Login from './Login';
+import Login from './Component/Login';
+import Check from './Component/Check';
+import SignUp from "./Component/SignUp";
+import Admin from './Component/Admin';
+import Forgottenpassword from './Component/Forgottenpassword';
+import PrivateComponent from './Component/PrivateComponent';
 
+//import Demo from './Component/Demo';
 
 function App() {
   return (
-    <div className="App"style={{backgroundColor:"skyblue", padding:10,margin:10,borderRadius:20, borderWidth:2, borderColor:"black", fontWeight: "normal" }}>
+    <div className="App" style={{ backgroundColor: "skyblue", padding: 10, margin: 10, borderRadius: 20, borderWidth: 2, borderColor: "black", fontWeight: "normal" }}>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/add' element={<Add />} />
+          <Route element={<PrivateComponent />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/add' element={<Add />} />
+            <Route path='/check' element={<Check />} />
+            <Route path='/admin' element={<Admin />} />
+          </Route>
+
           <Route path='/login' element={<Login />} />
+          <Route path='/forgottenpassword' element={<Forgottenpassword />} />
+          <Route path='/signup' element={<SignUp />} />
+          {/* <Route path='/demo' element={<Demo />} /> */}
 
         </Routes>
-
-
-
-
-
-
       </BrowserRouter>
 
 
