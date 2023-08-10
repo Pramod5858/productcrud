199074e06c786893883f47dd468ec509c61571cf
Pramod Mukane <108333259+Pramod5858@users.noreply.github.com>
2023-08-10 18:27:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+import Home from './Component/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from './Component/Navbar';
+import Add from './Component/Add';
+import Login from './Component/Login';
+import Check from './Component/Check';
+import SignUp from "./Component/SignUp";
+import Admin from './Component/Admin';
+import Forgottenpassword from './Component/Forgottenpassword';
+import PrivateComponent from './Component/PrivateComponent';
+import OtpVerify from './Component/OtpVerify';
+
+//import Demo from './Component/Demo';
+
+function App() {
+  return (
+    <div className="App" style={{ backgroundColor: "skyblue", padding: 10, margin: 10, borderRadius: 20, borderWidth: 2, borderColor: "black", fontWeight: "normal" }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route element={<PrivateComponent />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/add' element={<Add />} />
+            <Route path='/check' element={<Check />} />
+            <Route path='/admin' element={<Admin />} />
+          </Route>
+
+          <Route path='/login' element={<Login />} />
+          <Route path='/forgottenpassword' element={<Forgottenpassword />} />
+          <Route path='/signup' element={<SignUp />} />
+          {/* <Route path='/demo' element={<Demo />} /> */}
+          <Route path='/otpverify' element={<OtpVerify />} />
+        </Routes>
+      </BrowserRouter>
+
+
+    </div>
+  );
+}
+
+export default App;
